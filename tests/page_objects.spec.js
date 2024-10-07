c8d27c6159c45d8dee5f73b8158af1b18acb40ce
HusneShabbir <shabbirhusne447@gmail.com>
2024-10-07 16:13:28 +0530

    first commit

--- tests/page_objects.spec.js
+++ tests/page_objects.spec.js
@@ -10,13 +10,14 @@ test('cart', async ({ page }) => {
     const Login_page = poManeger.getLogin_page();
     await Login_page.navigate();
     await Login_page.login_method(user,pass);
+
     const Products = poManeger.getProducts();
     await Products.products_finalize(final_prod);
-    //prod_cart_page
     await Products.prod_cart_page();
+
     const Cart_page = poManeger.get_Cart_page();
     await Cart_page.cart_check_checkout(final_prod)
-
+    
     const Your_info = poManeger.get_Your_info();
     await Your_info.info_fill('apple', 'banana' , '0007');
     await Your_info.continue();
